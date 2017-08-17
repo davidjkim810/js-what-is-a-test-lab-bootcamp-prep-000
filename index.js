c1707587647f32af0aa0b9acc8b6bdfcf594e74a
David Kim <davidjkim810@gmail.com>
2017-08-17 03:05:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,41 +5,6 @@ var message = `Susan is ${height} inches tall`
 
 module.exports = {name, height, message}
 
-var word = 'bird'
-
-var a = 5
-var b = 2
-var c = 3
-var d = 'hello'
-var e = 'goodbye'
-
-
 function sayHello() {
   console.log('Hello!')
 }
-
-sayHello()
-
-function sayHelloToIsabel() {
-  console.log("Hello, Isabel!")
-}
-
-sayHelloToIsabel()
-
-function sayHelloToSofia() {
-  console.log("Hello, Sofia!")
-}
-
-sayHelloToSofia()
-
-function sayHelloToBrendan(){
-  console.log("Hello, Brendan!")
-}
-
-sayHelloToBrendan()
-
-function doSomething(thing) {
-  console.log(thing)
-}
-
-doSomething('anything')
